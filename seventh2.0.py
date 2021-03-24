90c0e7fc8e09e2f683a3f3fd54e92bb039ce6992
werble7 <60023966+werble7@users.noreply.github.com>
2021-03-24 12:37:34 -0300

    Delete seventh2.0.py

--- seventh2.0.py
+++
@@ -1,8 +0,0 @@
-px = float(input("insira a coordenada x do ponto p: "))
-py = float(input("insira a coordenada y do ponto p: "))
-qx = float(input("insira a coordenada x do ponto q: "))
-qy = float(input("insira a coordenada y do ponto q: "))
-import cmath
-distancia = (qx - px) ** 2 + (qy - py) ** 2
-raiz = distancia ** 0.5
-print("a distância é ", raiz)
